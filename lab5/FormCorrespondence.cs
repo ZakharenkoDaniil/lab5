b06004a15caf00141bd69f8c9c1dd5549fcf0a09
ZakharenkoDaniil <zakharenko1dany@gmail.com>
2021-12-08 19:10:59 +0300

    Добавьте файлы проекта.

---
+++ lab5/FormCorrespondence.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab5
+{
+    public partial class FormCorrespondence : Form
+    {
+        public FormCorrespondence()
+        {
+            InitializeComponent();
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "testdbDataSet.Korrespondenshin1". При необходимости она может быть перемещена или удалена.
+            this.korrespondenshin1TableAdapter.Fill(this.testdbDataSet.Korrespondenshin1);
+            this.textBox9.Text = (double.Parse(this.textBox4.Text)+double.Parse(this.textBox5.Text)+(this.textBox6.Text)).ToString();
+        }
+
+        private void bindingNavigatorMovePreviousItem_Click(object sender, EventArgs e)
+        {
+            this.textBox9.Text = (double.Parse(this.textBox4.Text) + double.Parse(this.textBox5.Text) + (this.textBox6.Text)).ToString();
+        }
+
+        private void bindingNavigatorMoveNextItem_Click(object sender, EventArgs e)
+        {
+            this.textBox9.Text = (double.Parse(this.textBox4.Text) + double.Parse(this.textBox5.Text) + (this.textBox6.Text)).ToString();
+        }
+
+        private void bindingNavigatorPositionItem_Click(object sender, EventArgs e)
+        {
+            this.textBox9.Text = (double.Parse(this.textBox4.Text) + double.Parse(this.textBox5.Text) + (this.textBox6.Text)).ToString();
+        }
+
+    }
+}
---
+++ lab5/FormSenders.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab5
+{
+    public partial class FormSenders : Form
+    {
+        public FormSenders()
+        {
+            InitializeComponent();
+        }
+
+        private void FormSenders_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "testdbDataSet.Otproviteliy1". При необходимости она может быть перемещена или удалена.
+            this.otproviteliy1TableAdapter.Fill(this.testdbDataSet.Otproviteliy1);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String connectionString = @"Data Source=localhost;Initial Catalog=testdb;User ID=sa;Password=Uqwerty333Q;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection cnn = new SqlConnection(connectionString);
+            cnn.Open();
+            SqlCommand cmd = cnn.CreateCommand();
+            cmd.CommandText = @"SELECT Трек_номер FROM Otproviteliy1 INNER JOIN Korrespondenshin1 ON Otproviteliy1.Код_отправителя = Korrespondenshin1.код_отправителя WHERE Паспортные_данные = " + this.textBox8.Text;
+            String res = "";
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                res += reader[0].ToString()+" ";
+            }
+            reader.Close();
+            this.textBox9.Text = res;
+            cnn.Close();
+        }
+    }
+}
